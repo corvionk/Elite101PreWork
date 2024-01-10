121aac43be2d15ff0d250aacc214291e9de8792d
CorvionKing <26895078-CorvionKing@users.noreply.replit.com>
2024-01-10 00:54:51 +0000

    Started and completed chatbot.
    Wrote basic code to formulate chatbot.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('more sauce on my burger yn')
+name = input('Hey, I\'m going to be your chatbot! What\'s your name? ')
+
+print('Hi ' + name + '! Nice to meet you!')
+age = input('How old are you? ')
+
+print('Please choose one of the following options: \n 1. Placeholder 1 \n 2. Placeholder 2 \n 3. Placeholder 3 \n 4. Exit. ')
+option = input('Enter the number of your option: ')
+
+if option == '4':
+  print('Goodbye!')
